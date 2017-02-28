3bd33ac3cee02b8a59147ad94a20f519f8c55445
Raptor2277 <stadnitchii@gmail.com>
2017-02-27 23:17:06 -0500

    Express autogeneration and custom middleware

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-var express = require("express")
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var addressParser = require('./remoteAddressParser');
 
-var app = express()
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-var apiRouter = require("./routes/api.js")
+var app = express();
 
-app.set("json spaces", 3)
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use("/api", apiRouter)
+// uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, './')));
+app.use(addressParser);
 
-app.get("/", function(req,res){
-	console.log("get request");
-    res.end("Hello World")
-})
+app.use('/', index);
+app.use('/users', users);
 
-var port = process.env.port || 8080;
-console.log("Up on port " + port);
-app.listen(port);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
