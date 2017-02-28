913c7026e7d19629b30929718a4735379e5d301d
Raptor2277 <stadnitchii@gmail.com>
2017-02-28 00:09:57 -0500

    Test

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './')));
-//app.use(addressParser);
+app.use(addressParser);
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.connection.remoteAddressIpv4);
+  console.log(req.connection.remoteAddress);
   res.end("hello world");
 });
 
