b63509ea31c2fd809fed968695de3655b0b6e381
Raptor2277 <stadnitchii@gmail.com>
2017-02-28 00:14:14 -0500

    Test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.connection.remoteAddress);
+  var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+
+  console.log(ip);
   res.end("hello world");
 });
 
