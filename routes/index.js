e028865d705b51c91c6e8cf86e097823c32af212
Raptor2277 <stadnitchii@gmail.com>
2017-02-28 00:04:04 -0500

    Error Testing

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.connection.remoteAddressIpv4);
-  res.render('index', { title: 'Express' });
+  res.end("hello world");
 });
 
 module.exports = router;
