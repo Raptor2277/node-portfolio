67f1acb8b1e9d940d32fea4418b75f6cb6449efa
Raptor2277 <stadnitchii@gmail.com>
2017-02-28 23:22:27 -0500

    Basic website (added handlebars)
    set everything up in terms of express and handlebars
    all that needs to be done is add the content

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var handlebars = require("express-handlebars");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var home = require('./routes/home');
+var resume = require('./routes/resume');
+var cpp = require('./routes/cpp');
+var csharp = require('./routes/csharp');
+var java = require('./routes/java');
+var arduino = require('./routes/arduino');
 
 var app = express();
 
 // view engine setup
+app.engine('hbs', handlebars({defaultLayout : 'layout', extname : 'hbs', 
+  layoutsDir : path.join(__dirname, 'views', 'layouts'),
+  partialsDir : path.join(__dirname, 'views', 'partials'),
+}));
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 app.use(logger('dev'));
@@ -20,10 +30,19 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/index', function(req, res, next)
+{
+  res.redirect('/');
+});
+
+app.use('/', home);
+app.use('/resume', resume);
+app.use('/cpp', cpp);
+app.use('/csharp', csharp);
+app.use('/java', java);
+app.use('/arduino', arduino);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
